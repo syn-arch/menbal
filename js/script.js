ebf80621b47064291489e8a824e5af8c5c230e3c
syn-arch <dyatna.id@gmail.com>
2020-11-09 23:28:43 +0700

    add page page.html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("loaded");
 
     // mobile navbar
     const navMoblie = document.querySelectorAll(".sidenav");
     M.Sidenav.init(navMoblie);
+
+    // toggle navbar
+    window.addEventListener("scroll", function () {
+        const heightTop = window.pageYOffset;
+        const nav = document.querySelector("nav");
+        const textColor = document.querySelectorAll(".toggle-scroll-color");
+
+        if (heightTop > 65) {
+            nav.classList.remove("transparent");
+            nav.classList.add("white");
+            textColor.forEach(function (el) {
+                el.classList.add("black-text");
+            });
+        } else {
+            nav.classList.add("transparent");
+            nav.classList.remove("white");
+            textColor.forEach(function (el) {
+                el.classList.remove("black-text");
+            });
+        }
+    });
 
 });
