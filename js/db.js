9fe01de5cb89228a842871fbcd8d764ea76cd896
syn-arch <dyatna.id@gmail.com>
2020-11-10 23:43:21 +0700

    add remove from indexdb

--- js/db.js
+++ js/db.js
@@ -1,57 +1,59 @@
 var dbPromised = idb.open("menbal", 1, function (upgradeDb) {
-  var teamsObjectStore = upgradeDb.createObjectStore("teams", {
-    keyPath: "id"
-});
-  teamsObjectStore.createIndex("name", "name", { unique: false });
+    var teamsObjectStore = upgradeDb.createObjectStore("teams", {
+        keyPath: "id"
+    });
+    teamsObjectStore.createIndex("name", "name", { unique: false });
 });
 
 function saveForLatter(team) {
-  dbPromised
-  .then(function (db) {
-    var tx = db.transaction("teams", "readwrite");
-    var store = tx.objectStore("teams");
-    store.add(team);
-    return tx.complete;
-})
-  .then(function () {
-    console.log("Artikel berhasil disimpan");
-});
+    dbPromised
+        .then(function (db) {
+            var tx = db.transaction("teams", "readwrite");
+            var store = tx.objectStore("teams");
+            store.add(team);
+            return tx.complete;
+        })
+        .then(function () {
+            console.log("Artikel berhasil disimpan");
+        });
 }
 
 function getAll() {
-  return new Promise(function (resolve,reject) {
-    dbPromised
-    .then(function(db){
-      var tx = db.transaction("teams", "readonly");
-      var store = tx.objectStore("teams");
-      return store.getAll();
-  })
-    .then(function(teams){
-      resolve(teams)
-  })
-})
+    return new Promise(function (resolve, reject) {
+        dbPromised
+            .then(function (db) {
+                var tx = db.transaction("teams", "readonly");
+                var store = tx.objectStore("teams");
+                return store.getAll();
+            })
+            .then(function (teams) {
+                resolve(teams)
+            })
+    })
 }
 
 function getById(id) {
-  return new Promise(function(resolve, reject) {
-    dbPromised
-    .then(function(db) {
-        var tx = db.transaction("teams", "readonly");
-        var store = tx.objectStore("teams");
-        console.log(id)
-        return store.get(id);
-    })
-    .then(function(team) {
-      resolve(team);
-  });
-});
+    return new Promise(function (resolve, reject) {
+        dbPromised
+            .then(function (db) {
+                var tx = db.transaction("teams", "readonly");
+                var store = tx.objectStore("teams");
+                return store.get(parseInt(id));
+            })
+            .then(function (team) {
+                resolve(team);
+            });
+    });
 }
 
 function deleteById(id) {
-  dbPromised
-  .then(function(db) {
-    var tx = db.transaction("teams", "readwrite");
-    var store = tx.objectStore("teams");
-    return store.delete(id);
-});
+    dbPromised
+        .then(function (db) {
+            var tx = db.transaction("teams", "readwrite");
+            var store = tx.objectStore("teams");
+            return store.delete(parseInt(id));
+        })
+        .then(function () {
+            console.log("Artikel berhasil dihapus");
+        });
 }
