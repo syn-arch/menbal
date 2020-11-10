ecd527d73971f473fa8dc213782f299b2d32f74a
syn-arch <dyatna.id@gmail.com>
2020-11-10 10:33:11 +0700

    go offline

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    // install & active service worker
+    if ("serviceWorker" in navigator) {
+        window.addEventListener("load", function () {
+            navigator.serviceWorker
+                .register("/sw.js")
+                .then(function () {
+                    console.log("pendaftaran serviceWorker berhasil");
+                })
+                .catch(function () {
+                    console.log("pendaftaran serviceWorker gagal")
+                });
+        });
+    } else {
+        console.log("serviceWorker belum didukung oleh browser ini");
+    }
+
     // mobile navbar
     const navMoblie = document.querySelectorAll(".sidenav");
     M.Sidenav.init(navMoblie);
@@ -28,5 +44,43 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         }
     });
+
+    // click action navbar
+    const navLink = document.querySelectorAll(".sidenav a, .topnav a");
+    navLink.forEach(function (el) {
+        el.addEventListener("click", function (e) {
+            // close side navbar
+            const sidenav = document.querySelector(".sidenav");
+            M.Sidenav.getInstance(sidenav).close();
+
+            // load content
+            const link = e.target.getAttribute("href").substr(1);
+            loadPage(link);
+        });
+    });
+
+    // load page
+
+    function loadPage(page) {
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                const content = document.querySelector("#app");
+                if (this.status == 200) {
+                    content.innerHTML = xhr.responseText;
+                } else if (this.status == 404) {
+                    content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
+                } else {
+                    content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
+                }
+            }
+        };
+        xhr.open("GET", `pages/${page}.html`, true);
+        xhr.send();
+    }
+
+    let page = window.location.hash.substr(1);
+    if (page == "") page = "home";
+    loadPage(page);
 
 });
---
+++ sw.js
@@ -0,0 +1,72 @@
+const CACHE_NAME = "menbal-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/pages/home.html",
+    "/pages/match.html",
+    "/pages/scores.html",
+    "/pages/team.html",
+    "/pages/favourite.html",
+    "/css/style.css",
+    "/node_modules/@fortawesome/fontawesome-free/css/all.min.css",
+    "/node_modules/@fortawesome/fontawesome-free/js/all.min.js",
+    "/node_modules/materialize-css/dist/css/materialize.min.css",
+    "/node_modules/materialize-css/dist/js/materialize.min.js",
+    "js/script.js",
+    "/favicon.ico",
+    "/img/ball.jpg",
+    "/img/field.jpg",
+    "/img/hero.jpg",
+    "/img/maps.svg",
+    "/img/offline.svg",
+    "/img/onboard.svg",
+    "/icon.png",
+    "/node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2",
+    "/node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff2",
+    "/sw.js",
+    "/manifest.json",
+    "https://fonts.googleapis.com/css2?family=Poppins&display=swap",
+    "/js/idb.js",
+    "/js/db.js",
+    "/js/api.js"
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+})
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys()
+            .then(function (cacheNames) {
+                return Promise.all(
+                    cacheNames.map(function (cacheName) {
+                        if (cacheName != CACHE_NAME) {
+                            console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
+    );
+})
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request, { cacheName: CACHE_NAME })
+            .then(function (response) {
+                if (response) {
+                    console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+                    return response;
+                }
+
+                console.log("ServiceWorker: Memuat aset dari server: ", event.request.url);
+                return fetch(event.request);
+            })
+    );
+});
